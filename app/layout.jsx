7d4740a82d1a88c5f7ca696162859023fe76cdd1
daliondev <d4lion.dev@gmail.com>
2023-12-07 11:15:51 -0500

    min(styles): Update dark mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,19 +2,12 @@
 
 import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react"
 import NavBar from "@/components/NavBar"
-import { mode } from "@chakra-ui/theme-tools"
+
+// Configuration of chakra
+import { config, styles } from "@/lib/theme"
 
 export default function RootLayout({ children }) {
-  const styles = {
-    global: (props) => ({
-      body: {
-        // Mode --> light, Dark
-        bg: mode("#e9e9e3", "#232524")(props),
-      },
-    }),
-  }
-
-  const theme = extendTheme({ styles })
+  const theme = extendTheme({ styles, config })
 
   return (
     <html lang="en">
