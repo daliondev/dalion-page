e295359b2c53d72c26b84d70e31396d0d93ead40
daliondev <d4lion.dev@gmail.com>
2023-12-12 01:09:34 -0500

    feat(new): metadata indexation

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,19 @@ export default function RootLayout({ children }) {
   const theme = extendTheme({ styles, config })
 
   return (
-    <html lang="en">
+    <html lang="en" dir="ltr">
+      <meta charSet="utf-8" />
+      <meta name="author" content="Daliondev" />
+      <meta
+        name="description"
+        content="portfolio of daliondev, works and more content about daliondev"
+      />
+      <meta
+        name="keywords"
+        content="daliondev, Daliondev, Dalion developer, daliondev.online, daliondev portfolio"
+      />
+      <link rel="icon" href="logo.png" sizes="any" />
+
       <body>
         <ChakraProvider theme={theme}>
           <Container maxW="xl" pt="1rem">
